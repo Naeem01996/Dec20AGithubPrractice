5a0c058c6698fd881c68bab056964be8f17cbd37
asdfgh <asdfgh@qwertyui>
2021-04-11 21:39:39 +0530

    [anky]naeem sir jii kya chal raha

---
+++ src/pack2/ample3.java
@@ -0,0 +1,9 @@
+package pack2;
+
+public class ample3
+{
+public static void main(String[] args)
+{
+	System.out.println("naeem sir how r u");
+}
+}
---
+++ src/pack2/sample2.java
@@ -0,0 +1,9 @@
+package pack2;
+
+public class sample2 
+{
+public static void main(String[] args) 
+{
+	System.out.println("sanjay sir automation");
+}
+}
